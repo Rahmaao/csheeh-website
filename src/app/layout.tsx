11bbafff80c4a1b202e6f2fdfd230b27c70cfc3c
Rahmaao <rahma.olaniyan@aun.edu.ng>
2023-10-19 10:31:13 +0100

    feat: footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { nunitoFont } from "./fonts";
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/Navbar/Navbar'
-import Footer from '@/components/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Center for School Health Education and Environment Hygiene",
@@ -24,13 +18,7 @@ export default function RootLayout({
     <html lang="en" className={nunitoFont.className}>
       <body className="w-full">
         <div className="min-h-[100vh] w-full">{children}</div>
-    <html lang="en">
-      <body className='font-[Inter] w-full'>
-        <div className="min-h-[100vh] w-full">
-        <Navbar />
-        {children}
         <Footer/>
-        </div>
       </body>
     </html>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,105 +1,109 @@
-import React from 'react'
-import facebook from '../../../public/images/facebook.svg'
-import instagram from '../../../public/images/instagram.svg'
-import logo from '../../../public/images/logo.svg'
-import Location from '../../../public/images/Location.svg'
-import phone from '../../../public/images/phone.svg'
-import Message from '../../../public/images/Message.svg'
+"use client";
+import React, { use } from "react";
+import facebook from "@/assets/icons/facebook.svg";
+import instagram from "@/assets/icons/instagram.svg";
+import logo from "@/assets/icons/logo.svg";
+import Location from "@/assets/icons/Location.svg";
+import phone from "@/assets/icons/phone.svg";
+import Message from "@/assets/icons/Message.svg";
 import Image from "next/image";
-import { Nunito } from 'next/font/google'
-import { Open_Sans } from 'next/font/google'
+import { nunitoFont } from "@/app/fonts";
+import { openSansFont } from "@/app/fonts";
+import tw, { styled, css } from "twin.macro";
+import Logo from "@/components/Logo" 
 
 const index = () => {
   return (
-    <div className='footer-div flex flex-col'>
-          <div className='main-footer grid md:grid-cols-2'>
-      <div className='card footer-section1 flex justify-center'>
-      <div className='footer-content1 flex flex-col justify-evenly'>
-        <div className='footer-component1 flex items-center'>
-            <div className='logo-img'>
-            <Image src={logo} alt="logo" className='icon-1'/>
+    <Root className={nunitoFont.className}>
+      <div className="main-footer grid md:grid-cols-2">
+        <div className="card footer-section1 flex justify-center">
+          <div className="footer-content1 flex flex-col justify-evenly">
+          <div className="w-[50%] h-[33%]">
+            <Logo/>
+            </div>            
+            <div className="footer-component2 flex">
+              <div className="icon-img">
+                <div className="img-icon">
+                  <Image src={Location} alt="Location" className="icon-1" />
+                </div>
+              </div>
+              <div className="text-content">
+                <p className="text2 text-xs">
+                  Centre for School Health Education and Environmental Hygiene
+                  (CSHEEH) <br />
+                  Sentosa Park, Near Fire Service, Zone A 1st Gate, Legislative
+                  Quarters, <br /> Apo, Abuja.
+                </p>
+              </div>
             </div>
-            <div className='logo-name flex items-center'>
-              <p className='text1 text-xs font-bold font-[Nunito]'>CENTRE FOR SCHOOL <br /> HEALTH EDUCATION AND ENVIRONMENTAL HYGIENE</p>
+            <div className="footer-component3 flex">
+              <div className="icon-img">
+                <div className="img-icon">
+                  <Image src={Message} alt="Message" className="icon-1" />
+                </div>
+              </div>
+              <div className="text-content">
+                <p className="text3 text-xs">info@csheehnigeria.org</p>
+              </div>
             </div>
-          </div>
-          <div className='footer-component2 flex'>
-            <div className='icon-img'>
-            <div className='img-icon'>
-            <Image src={Location} alt="Location" className='icon-1'/>
-            </div>
-            </div>
-            <div className='text-content'>
-              <p className='text2 text-xs'>Centre for School Health Education and Environmental Hygiene (CSHEEH) <br />
-Sentosa Park, Near Fire Service, Zone A 1st Gate, Legislative Quarters, <br /> Apo, Abuja.</p>
-            </div>
-          </div>
-           <div className='footer-component3 flex'>
-            <div className='icon-img'>
-            <div className='img-icon'>
-            <Image src={Message} alt="Message" className='icon-1'/>
-            </div>
-            </div>
-            <div className='text-content'>
-              <p className='text3 text-xs'>info@csheehnigeria.org</p>
-            </div>
-            </div>
-          <div className='footer-component4 flex'>
-            
-          <div className='icon-img'>
-            <div className='img-icon'>
-            <Image src={phone} alt="phone" className='icon-1'/>
-            </div>
-          </div>
-            <div className='text-content'>
-              <p className='text3 text-xs'>+234 803 787 1487; +234 805 356 3370</p>
-            </div>
-
-            </div>
-      </div>
-        
-
-      </div>
-      <div className='card footer-section2 flex justify-center items-center'>
-      <div className='footer-content2 flex flex-col items-end justify-between'>
-      <div className='footer-component5 flex justify-between'>
-            <div className='footer-nav-items'>
-              <a href="">ABOUT US</a>
-            </div>
-            <div className='footer-nav-items'>
-              <a href="">GET INVOLVED</a>
-            </div>
-            <div className='footer-nav-items'>
-              <a href="">MEDIA</a>
-            </div>
-            <div className='footer-nav-items'>
-              <a href="">CONTACT US</a>
+            <div className="footer-component4 flex">
+              <div className="icon-img">
+                <div className="img-icon">
+                  <Image src={phone} alt="phone" className="icon-1" />
+                </div>
+              </div>
+              <div className="text-content">
+                <p className="text3 text-xs">
+                  +234 803 787 1487; +234 805 356 3370
+                </p>
+              </div>
             </div>
           </div>
-          <div className='footer-component6 flex justify-between items-center'>
-            <div className='logo-img1'>
-            <Image src={facebook} alt="facebook" className='icon-1'/>
-
+        </div>
+        <div className="card footer-section2 flex justify-center items-center">
+          <div className="footer-content2 flex flex-col items-end justify-between">
+            <div className="footer-component5 flex justify-between">
+              <div className="footer-nav-items">
+                <a href="">ABOUT US</a>
+              </div>
+              <div className="footer-nav-items">
+                <a href="">GET INVOLVED</a>
+              </div>
+              <div className="footer-nav-items">
+                <a href="">MEDIA</a>
+              </div>
+              <div className="footer-nav-items">
+                <a href="">CONTACT US</a>
+              </div>
             </div>
-            <div className='logo-img1'>
-            <Image src={instagram} alt="instagram" className='icon-1'/>
+            <div className="footer-component6 flex justify-between items-center">
+              <div className="logo-img1">
+                <Image src={facebook} alt="facebook" className="icon-1" />
+              </div>
+              <div className="logo-img1">
+                <Image src={instagram} alt="instagram" className="icon-1" />
+              </div>
             </div>
-            </div>
+          </div>
+        </div>
       </div>
 
-</div>
+      <Copyright className={nunitoFont.className}>
+        <hr className="h-[0.2px] w-[95%] mx-auto mt-3 border-0 bg-[#a3a3a343]" />
+        <p className="text-[10px] font-thin tracking-wide">Copyright © CSHEEH 2023. All rights reserved.</p>
+      </Copyright>
+    </Root>
+  );
+};
 
-    </div>
-    
-    <div className='copyright-section'>
+export default index;
 
-</div>
+const Root = styled.div(() => [
+  tw`w-full h-[20em] flex flex-col bg-[#030426]`,
+  css`footer-div`,
+]);
 
-    </div>
+const Copyright = styled.div(() => [tw`flex flex-col justify-evenly items-center w-full h-[20%] text-gray-300`, css``]);
 
-  )
-}
-
-export default index
 
 
