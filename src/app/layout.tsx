1ff7bfb03fc327c8921f4ffad026dbcf7820d655
Rahmaao <rahma.olaniyan@aun.edu.ng>
2023-10-22 16:44:31 +0100

    feat: footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { nunitoFont } from "./fonts";
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/Footer'
