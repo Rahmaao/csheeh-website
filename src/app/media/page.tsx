331fb8338b05b52559a6d1c11aacd7af1b3eface
Rahmaao <rahma.olaniyan@aun.edu.ng>
2023-10-24 14:38:36 +0100

    feat: media page

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import MediaCards from '@/components/MediaCards'
+
+
 
 const Media = () => {
   return (
     <div>
       <h1>Media</h1>
+      <MediaCards/>
     </div>
   )
 }
