faf0a348e2ce896a963235bc12f261d06c16adc0
Femi Olatubosun | ƒa†3 <femi@zerocomplex.ai>
2023-10-25 22:20:56 +0100

    chore: update imports

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -4,6 +4,7 @@ import PageHero from "@/components/PageHero";
 import React from "react";
 import tw, { styled } from "twin.macro";
 import MediaImg from "@/assets/images/media-page.png";
+import MediaCards from "@/components/MediaCards";
 
 const Media = () => {
   return (
@@ -13,12 +14,11 @@ const Media = () => {
         subHeader="View and read more of our work in some of our featured news articles."
         image={MediaImg}
       />
- <MediaCards/>
+      <MediaCards />
     </Root>
   );
 };
 
 const Root = styled.div(() => [tw``]);
-
 
 export default Media;
--- src/components/MediaCards/index.tsx
+++ src/components/MediaCards/index.tsx
@@ -18,7 +18,6 @@ import { recoletasemiboldLocalFont } from "@/app/fonts";
 const index = () => {
   return (
     <Root>
-      <div className="get-involved-hero bg-green-400 h-[100vh]"></div>
       <MediaSection>
         <div className="media-text w-full h-[15%]   md:pl-[75px] flex md:justify-normal justify-center items-center">
           <div className="media-text-card w-[85%] lg:w-[50%] h-[80%] lg:h-[80%] md:h-[50%]  flex flex-col justify-center items-center md:items-start">
@@ -35,7 +34,11 @@ const index = () => {
         </div>
         <div className="media-cards w-full h-[85%] bg-[#ffff] flex flex-col justify-evenly items-center">
           <MediaCard>
-            <a target="_blank" href="https://nannews.ng/2023/10/19/organisation-calls-for-coordinated-efforts-in-pursuit-of-universal-hand-hygiene/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://nannews.ng/2023/10/19/organisation-calls-for-coordinated-efforts-in-pursuit-of-universal-hand-hygiene/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -54,7 +57,11 @@ const index = () => {
                 </span>
               </div>
             </a>
-            <a target="_blank" href="https://www.abujadigest.info/stories/education/ngo-donates-handwashing-facilities-to-fct-school/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://www.abujadigest.info/stories/education/ngo-donates-handwashing-facilities-to-fct-school/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -74,7 +81,11 @@ const index = () => {
                 </span>
               </div>
             </a>
-            <a target="_blank" href="https://www.abujadigest.info/stories/health/world-menstrual-hygiene-day-stakeholders-call-for-early-pre-menstrual-counseling/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://www.abujadigest.info/stories/health/world-menstrual-hygiene-day-stakeholders-call-for-early-pre-menstrual-counseling/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -96,7 +107,11 @@ const index = () => {
             </a>
           </MediaCard>
           <MediaCard>
-            <a target="_blank" href="https://www.ethiopianewsobserver.com/csheeh-unilever-partner-to-integrate-oral-hygiene-into-school-curricula-founder/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://www.ethiopianewsobserver.com/csheeh-unilever-partner-to-integrate-oral-hygiene-into-school-curricula-founder/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -116,7 +131,11 @@ const index = () => {
                 </span>
               </div>
             </a>
-            <a target="_blank" href="https://www.abujadigest.info/stories/health/stakeholders-call-for-improved-dental-health-infrastructure-in-primary-health-centers/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://www.abujadigest.info/stories/health/stakeholders-call-for-improved-dental-health-infrastructure-in-primary-health-centers/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -136,7 +155,11 @@ const index = () => {
                 </span>
               </div>
             </a>
-            <a target="_blank" href="https://www.abujadigest.info/stories/health/world-hepatitis-day-fct-residents-get-free-vaccination/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://www.abujadigest.info/stories/health/world-hepatitis-day-fct-residents-get-free-vaccination/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -157,7 +180,11 @@ const index = () => {
             </a>
           </MediaCard>
           <MediaCard>
-            <a target="_blank" href="https://www.abujadigest.info/stories/health/health-education-group-conducts-impact-assessment-of-hygiene-facilities/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://www.abujadigest.info/stories/health/health-education-group-conducts-impact-assessment-of-hygiene-facilities/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -169,14 +196,19 @@ const index = () => {
               </div>
               <div className="media-img-text h-[50%] lg:h-[25%] w-[50%] lg:w-[90%] lg:pt-4  flex flex-col items-center justify-evenly">
                 <p className="text-[10px] md:text-[13px] font-bold text-center">
-                Health Education: Group Conducts Impact Assessment of Hygiene Facilities
+                  Health Education: Group Conducts Impact Assessment of Hygiene
+                  Facilities
                 </p>
                 <span className="block text-[#058A51] text-[10px] md:text-[12px] font-semibold">
                   <p>MARCH 24, 2022</p>
                 </span>
               </div>
             </a>
-            <a target="_blank" href="https://sunnewsonline.com/world-oral-health-day-ngo-partners-with-dental-association-unilever-to-enlighten-pupils/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://sunnewsonline.com/world-oral-health-day-ngo-partners-with-dental-association-unilever-to-enlighten-pupils/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -188,14 +220,19 @@ const index = () => {
               </div>
               <div className="media-img-text h-[50%] lg:h-[25%] w-[50%] lg:w-[100%] lg:pt-4  flex flex-col items-center justify-evenly">
                 <p className="text-[10px] md:text-[13px] font-bold text-center">
-                World Oral Health Day: NGO partners with dental association, Unilever to enlighten pupils
+                  World Oral Health Day: NGO partners with dental association,
+                  Unilever to enlighten pupils
                 </p>
                 <span className="block text-[#058A51] text-[10px] md:text-[12px] font-semibold">
                   <p>MARCH 24, 2022</p>
                 </span>
               </div>
             </a>
-            <a target="_blank" href="https://globalhandwashing.org/global-handwashing-day/take-action/submit-events/view-events/csheeh-celebrates-global-handwashing-day-2017-in-nigeria/" className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center">
+            <a
+              target="_blank"
+              href="https://globalhandwashing.org/global-handwashing-day/take-action/submit-events/view-events/csheeh-celebrates-global-handwashing-day-2017-in-nigeria/"
+              className="media-card w-[100%] md:w-[85%] lg:w-[30%] h-[30%] lg:h-[90%]  flex lg:flex-col items-center"
+            >
               <div className="media-image h-full lg:h-[75%] w-[50%] lg:w-full  flex justify-center items-center">
                 <MediaImage>
                   <Image
@@ -207,7 +244,7 @@ const index = () => {
               </div>
               <div className="media-img-text h-[50%] lg:h-[25%] w-[50%] lg:w-[90%] lg:pt-4  flex flex-col items-center justify-evenly">
                 <p className="text-[10px] md:text-[13px] font-bold text-center">
-                CSHEEH celebrates Global Handwashing Day 2017 in Nigeria
+                  CSHEEH celebrates Global Handwashing Day 2017 in Nigeria
                 </p>
                 <span className="block text-[#058A51] text-[10px] md:text-[12px] font-semibold">
                   <p>OCTOBER 17, 2017</p>
@@ -228,7 +265,7 @@ const Root = styled.div(() => [tw`w-full h-[100vh] bg-slate-600`, css``]);
 const MediaSection = styled.div(() => [tw` h-[250vh]`, css``]);
 
 const MediaText1 = styled.div(() => [
-  tw`text-[22px] md:text-[27px] w-full h-[32%] tracking-wide text-center flex items-center md:justify-normal justify-center`,
+  tw`text-[22px] md:text-[27px] w-full h-[32%] tracking-wide text-center flex items-center md:justify-start justify-center`,
   css``,
 ]);
 
